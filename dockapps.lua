939343fba922c0ac7b242d4c0441a34cbc6812d3
childe <rmself@qq.com>
2024-12-28 21:03:24 +0800

    Rename var name

--- dockapps.lua
+++ dockapps.lua
@@ -1,25 +1,21 @@
 appChooser = hs.chooser.new(function (choice)
   if choice then
-    --print(choice.text)
     choice.win:focus()
   end
 end)
 
--- 获取 Dock 中的应用程序列表
 local function getWindows()
-  local windows = hs.window.allWindows()
+  local windows = {}
 
-  local apps = {}
-  for i, win in ipairs(windows) do
+  for i, win in ipairs(hs.window.allWindows()) do
     local item = {}
     item.text = win:title()
     item.win = win
-    table.insert(apps, 1, item)
+    table.insert(windows, 1, item)
   end
-  return apps
+  return windows
 end
 
--- 长按 opt 键时显示应用程序列表
 hs.hotkey.bind({"cmd"}, "0", function()
   local windows = getWindows()
   print(#windows, 'windows')
